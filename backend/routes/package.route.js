3fa5260b332631a76e7b5b886523cc2bf9a14bcb
DhananjayaLakshan <abey.dhananjaya@gmail.com>
2024-03-31 16:40:32 +0530

    remove middleware

--- backend/routes/package.route.js
+++ backend/routes/package.route.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const { createPackage, getPackage, updatePackage, deletePackage } = require('../controllers/package.controller')
-const uploadMiddleware = require('../utils/MulterMiddleware')
 const router = express.Router()
 
 router.post('/', createPackage)
